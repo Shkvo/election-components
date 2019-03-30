149112dedcb673510eb4f1d3709ab0d95737ae09
shkvo <dimashkvo@gmail.com>
2019-03-30 13:16:18 +0200

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import { cn } from '@bem-react/classname';
+import './App.scss';
+
+const cnApp = cn('App');
+
+const App = () => {
+
+  return (
+    <div className={cnApp()}>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+};
+
+export default App;
